004c221ac88387e02923a4044ef533bdb2390d7c
yuumillar <araera111@gmail.com>
2022-08-21 16:12:10 +0900

    feat(KeyOperation.tsx): 右で戻る、左で進むを実装

--- src/features/KeyOperation/components/KeyOperation.tsx
+++ src/features/KeyOperation/components/KeyOperation.tsx
@@ -1,11 +1,21 @@
 import { useCallback, useEffect } from 'react';
+import { useViewerStore } from '../../Viewer/stores/viewerStore';
 
 export const KeyOperation = () => {
-  const keyFunction = useCallback((event: KeyboardEvent) => {
-    if (event.key) {
-      console.log({ key: event.key });
-    }
-  }, []);
+  const next = useViewerStore((state) => state.nextPage);
+  const prev = useViewerStore((state) => state.prevPage);
+  const keyFunction = useCallback(
+    (event: KeyboardEvent) => {
+      if (event.key === 'ArrowRight') {
+        prev(1);
+      }
+
+      if (event.key === 'ArrowLeft') {
+        next(1);
+      }
+    },
+    [next, prev]
+  );
   useEffect(() => {
     document.addEventListener('keydown', keyFunction, false);
   }, [keyFunction]);
--- src/features/Viewer/stores/viewerStore.ts
+++ src/features/Viewer/stores/viewerStore.ts
@@ -3,9 +3,11 @@ import create from 'zustand';
 type ViewerState = {
   page: number;
   nextPage: (by: number) => void;
+  prevPage: (by: number) => void;
 };
 
 export const useViewerStore = create<ViewerState>()((set) => ({
   page: 1,
-  nextPage: (by) => set((state) => ({ page: state.page + by }))
+  nextPage: (by) => set((state) => ({ page: state.page + by })),
+  prevPage: (by) => set((state) => ({ page: state.page - by }))
 }));
