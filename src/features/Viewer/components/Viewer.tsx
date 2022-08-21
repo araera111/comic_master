7f7b14664bd5dfb3110d988a94f1b4ce1204973f
yuumillar <araera111@gmail.com>
2022-08-21 15:29:33 +0900

    feat:

---
+++ src/features/Viewer/components/Viewer.tsx
@@ -0,0 +1,12 @@
+export const Viewer = () => {
+  const a = '';
+  return (
+    <div className="mx-auto">
+      <img
+        src="https://junpei-sugiyama.com/wp-content/uploads/2022/04/slick-full-screen.jpg"
+        alt=""
+        className="object-contain h-screen w-screen"
+      />
+    </div>
+  );
+};
