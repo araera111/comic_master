6db483f15104ac6ad3adeb05b1ec765e498a98c5
yuumillar <araera111@gmail.com>
2022-08-22 22:05:18 +0900

    feat(FileDropZone.tsx): zip対応
    feat #6

--- src/features/DropOperation/utils/dropOperationUtil.ts
+++ src/features/DropOperation/utils/dropOperationUtil.ts
@@ -1,1 +1,5 @@
 export const uint8ArrayToBase64 = (uint8Array: Uint8Array) => String.fromCharCode(...uint8Array);
+
+export const enableExtnames = ['jpg', 'jpeg', 'png'];
+
+export const getExtName = (path: string): string => path.split('.').pop() ?? '';
