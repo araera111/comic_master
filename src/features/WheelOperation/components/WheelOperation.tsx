ec99b047de4bfc00228eac1e1371cb91f0110072
yuumillar <araera111@gmail.com>
2022-08-28 19:27:08 +0900

    feat(WheelOperation.tsx): ひとまず完成したけど、ページ移動の部分をつくらないといかんぞよ

--- src/features/WheelOperation/components/WheelOperation.tsx
+++ src/features/WheelOperation/components/WheelOperation.tsx
@@ -1,19 +1,37 @@
 import { ReactNode } from 'react';
 import ReactScrollWheelHandler from 'react-scroll-wheel-handler';
+import { useViewerStore } from '../../Viewer/stores/viewerStore';
 
 type WheelOperationProps = {
   children: ReactNode;
 };
 export const WheelOperation = ({ children }: WheelOperationProps) => {
-  const up = () => {
-    console.log('scroll up');
+  const next = useViewerStore((state) => state.nextPage);
+  const prev = useViewerStore((state) => state.prevPage);
+  const up = (e: WheelEvent) => {
+    prev();
+    console.log(e);
+    console.log('up');
   };
+  const down = (e: WheelEvent) => {
+    next();
+    console.log(e);
 
-  const down = () => {
-    console.log('scroll down');
+    console.log('down');
   };
   return (
-    <ReactScrollWheelHandler upHandler={() => up()} downHandler={() => down()}>
+    <ReactScrollWheelHandler
+      upHandler={(e) => up(e)}
+      downHandler={(e) => down(e)}
+      timeout={0}
+      /*
+        stability: number,
+        sensitivity: number,
+        tolerance: number,
+        delay: number
+      */
+      wheelConfig={[0, 100, 0.01, 0]}
+    >
       {children}
     </ReactScrollWheelHandler>
   );
