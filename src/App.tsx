59a80dfdceb0424b5c059e88c779491463f4f271
yuumillar <araera111@gmail.com>
2022-08-28 19:06:25 +0900

    feat(WheelOperation.tsx): とりあえずtsxを作成しｔした

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { FileDropZone } from './features/DropOperation/components/FileDropZone';
 import { KeyOperation } from './features/KeyOperation/components/KeyOperation';
 import { Viewer } from './features/Viewer/components/Viewer';
+import { WheelOperation } from './features/WheelOperation/components/WheelOperation';
 
 export const App = () => (
   <div className="h-screen w-screen">
@@ -8,5 +9,6 @@ export const App = () => (
       <Viewer />
     </FileDropZone>
     <KeyOperation />
+    <WheelOperation />
   </div>
 );
---
+++ src/features/WheelOperation/components/WheelOperation.tsx
@@ -0,0 +1,4 @@
+export const WheelOperation = () => {
+  const a = '';
+  return null;
+};
