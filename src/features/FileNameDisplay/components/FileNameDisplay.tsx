0872eb7202335c9130862b6a2cb38824d454ee4d
yu <araera111@gmail.com>
2022-08-30 22:21:48 +0900

    feat(FileNameDisplay.tsx): spreadStartRightのときに2行で表示するように調整
    feat #13

--- src/features/FileNameDisplay/components/FileNameDisplay.tsx
+++ src/features/FileNameDisplay/components/FileNameDisplay.tsx
@@ -1,11 +1,25 @@
 import { isEmpty } from 'rambda';
+import { match } from 'ts-pattern';
 import { useViewerStore } from '../../Viewer/stores/viewerStore';
 
 export const FileNameDisplay = () => {
   const pageItems = useViewerStore((state) => state.pageItems);
   const page = useViewerStore((state) => state.page);
+  const mode = useViewerStore((state) => state.mode);
   const isShowFileName = useViewerStore((state) => state.isShowFileName);
-  return isShowFileName && !isEmpty(pageItems) ? (
-    <div className="z-50 absolute bottom-0 bg-slate-400 opacity-70">{pageItems[page].fileName}</div>
-  ) : null;
+  return match(mode)
+    .with('single', () =>
+      isShowFileName && !isEmpty(pageItems) ? (
+        <div className="z-50 absolute bottom-0 bg-slate-400 opacity-70">{pageItems[page].fileName}</div>
+      ) : null
+    )
+    .with('spreadStartRight', () =>
+      isShowFileName && !isEmpty(pageItems) ? (
+        <>
+          <div className="z-50 text-sm absolute bottom-6 bg-slate-400 opacity-70">{pageItems[page + 1].fileName}</div>
+          <div className="z-50 text-sm absolute bottom-0 bg-slate-400 opacity-70">{pageItems[page].fileName}</div>
+        </>
+      ) : null
+    )
+    .otherwise(() => null);
 };
