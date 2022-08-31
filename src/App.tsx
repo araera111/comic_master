3d8a610d77bad793e999e387cbe96e101bee95a0
yu <araera111@gmail.com>
2022-08-31 21:52:00 +0900

    feat(LoaderEffect.tsx): ローディングエフェクトを追加
    feat #30

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { FileDropZone } from './features/DropOperation/components/FileDropZone';
 import { FileNameDisplay } from './features/FileNameDisplay/components/FileNameDisplay';
 import { KeyOperation } from './features/KeyOperation/components/KeyOperation';
+import { LoadingEffect } from './features/LoaderEffect/components/LoaderEffect';
 import { PageRange } from './features/PageRange/components/PageRange';
 import { Viewer } from './features/Viewer/components/Viewer';
 import { WheelOperation } from './features/WheelOperation/components/WheelOperation';
@@ -9,6 +10,7 @@ export const App = () => (
   <WheelOperation>
     <div className="h-screen w-screen">
       <PageRange />
+      <LoadingEffect />
       <FileNameDisplay />
       <FileDropZone>
         <Viewer />
---
+++ src/features/LoaderEffect/components/LoaderEffect.tsx
@@ -0,0 +1,17 @@
+import { RotatingSquare } from 'react-loader-spinner';
+
+export const LoadingEffect = () => {
+  const a = '';
+  return (
+    <RotatingSquare
+      height="100"
+      width="100"
+      color="#4fa94d"
+      ariaLabel="rotating-square-loading"
+      strokeWidth="4"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible
+    />
+  );
+};
